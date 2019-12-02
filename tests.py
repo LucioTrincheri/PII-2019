6b4d87118f6fe5d3fec88de8d4ccc84fe81a3242
LucioTrincheri <lucio4852071@gmail.com>
2019-12-02 14:55:37 -0300

    Agrego los tests

--- tests.py
+++ tests.py
@@ -12,6 +12,7 @@ def test_parserArchivo():
   assert ini == (0, 0)
   assert fin == (4, 3)
 
+
 def test_heuristica():
   test = open("test_files/test_heuristica.txt", "r")
   resul = open("test_files/resultado_heuristica.txt", "r")
@@ -21,8 +22,34 @@ def test_heuristica():
   assert heuristica(eval(test_lines[2]), eval(test_lines[3])) == eval(resul_lines[1])
   assert heuristica(eval(test_lines[4]), eval(test_lines[5])) == eval(resul_lines[2])
 
-#def test_fueraDeRango():
 
-#def test_obtenerVecinos():
+def test_fueraDeRango():
+  test = open("test_files/test_fueraDeRango.txt", "r")
+  resul = open("test_files/resultado_fueraDeRango.txt", "r")
+  test_lines = test.readlines() # Tamaño y tuplas a evaluar
+  resul_lines = resul.readlines() # Retorno de funcion
+  assert fueraDeRango(eval(test_lines[1])[0], eval(test_lines[1])[1], eval(test_lines[0])) == eval(resul_lines[0])
+  assert fueraDeRango(eval(test_lines[2])[0], eval(test_lines[2])[1], eval(test_lines[0])) == eval(resul_lines[1])
+  assert fueraDeRango(eval(test_lines[3])[0], eval(test_lines[3])[1], eval(test_lines[0])) == eval(resul_lines[2])
+  assert fueraDeRango(eval(test_lines[4])[0], eval(test_lines[4])[1], eval(test_lines[0])) == eval(resul_lines[3])
 
-#def test_aStar():
+
+def test_obtenerVecinos():
+  lab, ini, fin = parserArchivo("test_files/test_parserArchivo.txt")
+  test = open("test_files/test_obtenerVecinos.txt", "r")
+  resul = open("test_files/resultado_obtenerVecinos.txt", "r")
+  test_lines = test.readlines() # Tupla a buscar vecinos (posiciones de array, osea x - 1, y - 1)
+  resul_lines = resul.readlines() # Lista de vecinos (posiciones de array, osea x - 1, y - 1)
+  size = 6 #(tamaño de los arrays, osea tamaño real - 1)
+  assert obtenerVecinos(eval(test_lines[0]), lab, size) == eval(resul_lines[0])
+  assert obtenerVecinos(eval(test_lines[1]), lab, size) == eval(resul_lines[1])
+  assert obtenerVecinos(eval(test_lines[2]), lab, size) == eval(resul_lines[2])
+
+
+def test_aStar():
+  test = open("test_files/test_aStar.txt", "r")
+  resul = open("test_files/resultado_aStar.txt", "r")
+  test_lines = test.readlines()
+  resul_lines = resul.readlines()
+  (ini, fin, lab) = eval(test_lines[0])
+  assert aStar(ini, fin, lab) == eval(resul_lines[0])
